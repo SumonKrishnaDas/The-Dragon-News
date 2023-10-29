5221bec40af70d9dfed7f1c7fb0d0219a0ef5a50
SumonKrishnaDas <sumondash51583@gmail.com>
2023-10-29 17:24:22 +0600

    some change in right side

--- src/Componentes/Pages/LeftSide/Left.jsx
+++ src/Componentes/Pages/LeftSide/Left.jsx
@@ -21,13 +21,18 @@ fetch('http://localhost:5000/catagories')
     return (
         <div>
 
-<h4>All catagories  </h4>
-<div className='nationalNews'><h4  className='News'> National News </h4> </div>
+<h4 className='mb-4'>All catagories  </h4>
+<div className='nationalNews mb-4'><h4  className='News'> National News </h4> </div>
+<div className='d-flex  justify-content-center '>
 
-{
-
-  catagories.map( catagory => <p key={catagory.id}><Link to={`/category/${catagory.id}`} className='text-decoration-none  text-black' > {catagory.name} </Link> </p>)
-}
+<div className='d-block'>
+  
+  {
+  
+  catagories.map( catagory => <p  key={catagory.id}><Link to={`/category/${catagory.id}`} className='text-decoration-none  text-black' > {catagory.name} </Link> </p>)
+  }
+  </div>
+</div>
 
 
 
